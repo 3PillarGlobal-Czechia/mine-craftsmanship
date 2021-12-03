1f471a1a37106f397c10d418f3c9ba5f8dc97779
Robert Žerdík <thezerda@users.noreply.github.com>
2021-12-03 13:23:09 +0100

    Initial commit

---
+++ src/main/java/com/threepillarglobal/craftsmanship/Craftsmanship.java
@@ -0,0 +1,10 @@
+package com.threepillarglobal.craftsmanship;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Craftsmanship implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        
+    }
+}
